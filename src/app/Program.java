353e5e9fea1ca8e0c7d3f1c709b010b26e4b3915
Italo <gabriel.italo.contato@gmail.com>
2023-09-06 11:10:49 -0300

    First class: position

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,10 +1,13 @@
 package app;
 
+import boardGame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Position ps = new Position(5, 10);
+		System.out.println(ps);
 	}
 
 }
