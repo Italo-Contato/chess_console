b21420f585d4b53ad538206e0c761ca2f7f3ac3b
Italo <gabriel.italo.contato@gmail.com>
2023-09-12 09:19:29 -0300

    BoardExcepetion and defensive programming

--- src/boardGame/Board.java
+++ src/boardGame/Board.java
@@ -2,47 +2,67 @@ package boardGame;
 
 public class Board {
 	private int rows;
-	private int colunm;
+	private int columns;
 	private Piece[][] pieces;
 
-	public Board(int rows, int colunm) {
-
+	public Board(int rows, int colunms) {
+		if (rows < 1 || colunms < 1) {
+			throw new BoardException("there must be at least one row and one column");
+		}
 		this.rows = rows;
-		this.colunm = colunm;
-		this.pieces = new Piece[rows][colunm];
+		this.columns = colunms;
+		this.pieces = new Piece[rows][colunms];
 	}
 
 	public int getRows() {
 		return rows;
 	}
 
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
-
 	public int getColunm() {
-		return colunm;
+		return columns;
 	}
 
-	public void setColunm(int colunm) {
-		this.colunm = colunm;
+	public boolean positionExist(int row, int column) {
+		return row >= 0 && row < rows && column >= 0 && column < columns;
+	}
+
+	public boolean positionExist(Position position) {
+		return positionExist(position.getRow(), position.getCollumn());
+	}
+
+	public boolean thereIsAPicie(Position position) {
+		if(!positionExist(position)) {
+			throw new BoardException("position not on board");
+		}		
+		return piece(position) != null;
 	}
 
 	public Piece piece(int row, int column) {
+		if (!positionExist(row, column)) {
+			throw new BoardException("this position not existe on board");
+		}
 		return pieces[row][column];
 	}
 
 	public Piece piece(Position position) {
+		if (!positionExist(position)) {
+			throw new BoardException("this position not existe on board");
+		}
 		return pieces[position.getRow()][position.getCollumn()];
 	}
 
 	public Piece Piece(Position position) {
 		return pieces[position.getRow()][position.getCollumn()];
 	}
+
 	public Piece placePiece(Piece piece, Position position) {
+		if (thereIsAPicie(position)) {
+			throw new BoardException("There is already a piece on position  ");
+
+		}
 		pieces[position.getRow()][position.getCollumn()] = piece;
 		piece.position = position;
 		return null;
-		
+
 	}
 }
